ee6fd06b9e5f50c1d7b2f3166392d22d40b71a29
vkchan715 <boyzkeit@gmail.com>
2024-06-02 15:17:16 +0800

    update

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 $(document).ready( function() {
   $("#front").click(function(){
     $("#inner").addClass("flip");
-    console.log(12);
   });
 
   $("#back").click(function(){
@@ -10,83 +9,22 @@ $(document).ready( function() {
       $("#back").removeClass("open")
     }else{
       $("#back").addClass("open");
+
+      setTimeout(function(){
+        $(".content-con").removeClass("hide");
+      },300);
+      
     }
-  
-    console.log(121);
+  });
+
+  $("#btnclose").click(function(){
+    $(".content-con").addClass("hide");
+    setTimeout(function(){
+          $("#back").removeClass("open");
+    },300);
+    setTimeout(function(){
+      $("#inner").removeClass("flip");
+    },500);
   });
 });
-// setTimeout(() => {
-//   setTimeout(() => {
-//     const card = document.getElementById("flip-card-inner");
-//     card.style.transform = "rotateY(180deg)";
-//   }, 1500);
-// }, 1000);
 
-// setTimeout(() => {
-//   setTimeout(openEnvelope, 1000);
-// }, 2000);
-
-// function openEnvelope() {
-//   setInterval(() => {
-//     document.getElementById("one").style.transform = "rotate(180deg)";
-//     document.getElementById("companyName").style.display = "none";
-//   }, 2000);
-
-//   setTimeout(letterUp, 2000);
-// }
-
-// function letterUp() {
-//   const letter = document.getElementById("letter");
-//   const one = document.getElementById("one");
-//   // const envelope = document.getElementById("envelope");
-
-//   one.style.zIndex = 1;
-//   //envelop
-//   letter.style.zIndex = 2;
-//   let i = 0;
-//   let id = null;
-//   clearInterval(id);
-
-//   id = setInterval(() => {
-//     if (i == 500) {
-//       clearInterval(id);
-//     } else {
-//       letter.style.top = -i + "px";
-//       i++;
-//     }
-//   }, 5);
-
-//   setTimeout(letterDown, 3000);
-// }
-
-// function letterDown() {
-//   const letter = document.getElementById("letter");
-//   const card = document.getElementById("flip-card");
-
-//   letter.style.top = -500 + "px";
-//   letter.style.zIndex = 4;
-
-//   let i = 0;
-//   let id = null;
-//   clearInterval(id);
-
-//   id = setInterval(() => {
-//     if (i == 100) {
-//       // lastPos = -i;
-//       clearInterval(id);
-//     } else {
-//       letter.style.top = -400 + i * 5 + "px";
-//       letter.style.transform = "rotate(" + -i / 18 + "deg)";
-
-//       card.style.transform = "rotate(" + i / 18 + "deg)";
-
-//       i++;
-//     }
-//   }, 10);
-
-//   setTimeout(() => {
-//     const popUp = document.getElementById("popUp");
-//     popUp.style.display = "block";
-//     popUp.style.zIndex = 5;
-//   }, 2000);
-// }
